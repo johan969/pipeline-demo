f3c1d3571fef17c90371db5c06c75a707e3453d0
johan <johan.persson@iths.se>
2026-02-12 13:11:32 +0100

    initial commit

---
+++ src/main/java/se/iths/johan/pipelinedemo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package se.iths.johan.pipelinedemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String hello() {
+        return "home";
+
+    }
+}
