3cce080db71936928fce0f05014462445024d77e
johan <johan.persson@iths.se>
2026-02-12 10:17:58 +0100

    initial commit

---
+++ src/test/java/se/iths/johan/pipelinedemo/PipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.johan.pipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
